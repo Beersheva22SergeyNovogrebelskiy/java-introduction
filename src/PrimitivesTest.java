6a550598862d54da9fbdeed12634c9aaf91d0359
Sergey Novogrebelskiy <sznovo@yandex.ru>
2022-10-30 20:03:58 +0200

    initial code

---
+++ src/PrimitivesTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrimitivesTest {
+
+	@Test
+	@Disabled
+	void dataTypeTest() {
+		int b = 10;
+		short a = 20;
+		char c = 'd';
+		long l = (long) 10.1;
+		byte b1 = (byte) 1000;
+		a = (short) l;
+		float f = 10.2f;
+		int b2 = 0xffffffff;
+		assertEquals(10, a);
+	}
+
+	@Test
+	@Disabled
+	void operatotTest() {
+		int number = 123;
+		assertEquals(3, getTirdDigit(number));
+		assertEquals(2, getSecondDigit(number));
+		assertEquals(1, getFirstDigit(number));
+	}
+
+	private Integer getFirstDigit(int number) {
+		return number / 100 % 10;
+	}
+
+	private Integer getSecondDigit(int number) {
+		return number / 10 % 10;
+	}
+
+	private Integer getTirdDigit(int number) {
+		return number % 10;
+	}
+	
+}
