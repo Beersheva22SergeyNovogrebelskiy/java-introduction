8cf0b0668253dfc21697176419ae909a493cbb3d
Sergey Novogrebelskiy <sznovo@yandex.ru>
2022-11-20 21:50:53 +0200

    Completed homework of 17 November

---
+++ src/ObjectTests.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ObjectTests {
+
+	@Test
+	void wrapperTest() {
+		
+		Integer a = 10;
+		Integer b = 11;
+		assertEquals(++a , b);
+		Integer c = 201;
+		Integer d = 201;
+		assertEquals(c, d);
+		assertTrue(a == b);
+		assertFalse(c == d);
+		assertTrue(c.equals(d));
+	}
+	@Test
+	void stringTest() {
+		String hello = "hello";
+		char[] helloAr = hello.toCharArray();
+		assertEquals('h', helloAr[0]);
+		//System.out.println((byte)helloAr[0]);
+		assertEquals(5, hello.length());
+	}
+	
+	@Test
+	void isAnagramTest() {
+		String word = "yellow";
+		assertTrue(Strings.isAnagram(word, "loweyl"));
+		assertTrue(Strings.isAnagram(word, "elolyw"));
+		assertTrue(Strings.isAnagram(word, "wolley"));
+		assertTrue(Strings.isAnagram(word, "loleyw"));
+		assertFalse(Strings.isAnagram(word,""));
+		assertFalse(Strings.isAnagram(word, "yellob"));
+		assertFalse(Strings.isAnagram(word,"yello"));
+		assertFalse(Strings.isAnagram(word,"yelllo"));
+	}
+	
+	@Test
+	void sortStringNumbersTest() {
+	String[] array1 = { "5", "-37", "15", "2", "22", "120", "118", "-110", "5", "2", "1", "0", "127", "-128", "1", "5" };
+	String[] array2 = { "-128", "-110", "-37", "0", "1", "1", "2", "2", "5", "5", "5", "15", "22", "118", "120", "127" };
+	assertArrayEquals(array2, Strings.sortStringNumbers(array1));
+	}
+
+}
---
+++ src/Strings.java
@@ -0,0 +1,56 @@
+public class Strings {
+/**
+ * 
+ * @param str1
+ * @param str2
+ * @return true if str2 is an anagram of str1
+ * complexity O[N] two passes
+ * only one additional array
+ * only symbols or English letters
+ * 1. the same length
+ * 2. the same symbols just in different order
+ */
+	public static boolean isAnagram(String str1, String str2) {
+		boolean res = false;
+		int count = str1.length();
+		int[] helper = new int[127];
+		char[] str1Ar = str1.toCharArray();
+		char[] str2Ar = str2.toCharArray();
+		if(str1.length() == str2.length()) {
+			for(int i = 0; i < str1Ar.length; i++) {
+				helper[(byte)str1Ar[i]]++;
+			}
+		}
+		for(int i = 0; i < str2Ar.length; i++) {
+			if(helper[str2Ar[i]] > 0) {
+				helper[str2Ar[i]]--;
+				count--;
+			}
+		}
+		if(count == 0) {
+			res = true;
+		}
+	return res;
+}
+	
+	public static String[] sortStringNumbers(String[] arString) {
+		String[] res = new String[arString.length];
+		int[] helper = new int[256];
+		int index = 0;
+		String temp;
+		for(int i = 0; i < arString.length; i++) {
+			helper[Integer.parseInt(arString[i])+128]++;
+		}
+		for(int i = 0; i < helper.length; i++) {
+			if(helper[i] > 0) {
+				while(helper[i] > 0) {
+					temp = Integer.toString(i - 128);
+					res[index] = temp;
+					index++;
+					helper[i]--;
+				}
+			}
+		}
+		return res;
+	}
+}
